b6a579e87197a14e09dcd2fef4635dc12934f162
demonbro <52437128+demonbro-dev@users.noreply.github.com>
2025-08-14 01:19:15 +0800

    Return 0 when an instance is already running

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 {
     const QString uniqueKey = "RandPicker_UniqueKey_" + QCoreApplication::applicationName();
     if (isAlreadyRunning(uniqueKey)) {
-        return 1;
+        return 0;
     }
 
     QApplication a(argc, argv);
